731d4b80cdcfb4bcab7d09b84748156a4ed9f656
thinhnw <thinhnv0412@gmail.com>
2021-06-07 10:18:13 +0700

    Initial Commit

---
+++ GeometryExample/Cylinder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    public class Cylinder
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+
+        public double BaseArea { get; set; }
+        public double LateralArea { get; set; }
+        public double TotalArea { get; set; }
+        public double Volume { get; set; }
+
+        public Cylinder()
+        {
+        }
+
+        public Cylinder(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public void Process()
+        {
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+
+        public string Result()
+        {
+            return "Radius: " + Radius + ", Height: " + Height + "\n" +
+                   "Base: " + BaseArea + " | Lateral: " + LateralArea + " | Total: " + TotalArea + " | Volume: " +
+                   Volume;
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cylinder = new Cylinder();
+            Console.WriteLine("Enter the dimensions of the cylinder");
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Radius: ");
+                    cylinder.Radius = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Height: ");
+                    cylinder.Height = Convert.ToDouble(Console.ReadLine());
+                    cylinder.Process();
+                    Console.WriteLine(cylinder.Result());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
